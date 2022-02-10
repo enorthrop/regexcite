a24c5462c92ce828b67f9be428654e843db52605
Ellie <north266@umn.edu>
2022-02-10 10:37:54 -0600

    Updated 2nd function

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,8 +1,7 @@
 #' Using a function from another package
 #'
 #' @param string A vector
-#' @param pattern What to split on
-#' @param n Max number of splits
+#' @inheritParams stringr::str_split
 #'
 #' @return A character vector
 #' @export
@@ -10,6 +9,10 @@
 #' @examples
 #' x <- "alfa,bravo,charlie,delta"
 #' str_split_one(x, pattern = ",")
+#' str_split_one(x, pattern = ",", n = 2)
+#'
+#' y <- "192.168.0.1"
+#' str_split_one(y, pattern = stringr::fixed("."))
 str_split_one <- function(string, pattern, n = Inf) {
   stopifnot(is.character(string), length(string) <= 1)
   if (length(string) == 1) {
